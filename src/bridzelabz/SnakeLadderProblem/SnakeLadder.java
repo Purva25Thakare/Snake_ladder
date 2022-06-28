82fb584f0f257eadafe7f30f5f595f65833b79e6
Purva25Thakare <thakarepurva25@gmail.com>
2022-06-28 14:11:14 +0530

    Added:Generate random value on die

--- src/bridzelabz/SnakeLadderProblem/SnakeLadder.java
+++ src/bridzelabz/SnakeLadderProblem/SnakeLadder.java
@@ -5,9 +5,9 @@ import java.util.Random;
 public class SnakeLadder {
 	public static void main(String[] args) {
 	System.out.println("Start at 0");
-	//Random rm=new Random();
-	//int die=rm.nextInt(6)+1;
-	//System.out.println("The value on die is:" +die);
+	Random rm=new Random();
+	int die=rm.nextInt(6)+1;
+	System.out.println("The value on die is:" +die);
 	
 	}
 }
