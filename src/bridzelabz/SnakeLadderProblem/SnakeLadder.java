07935e624da35b4441529090051965626d2bd969
Purva25Thakare <thakarepurva25@gmail.com>
2022-06-28 14:08:43 +0530

    Added:Game Started at zero position

---
+++ src/bridzelabz/SnakeLadderProblem/SnakeLadder.java
@@ -0,0 +1,13 @@
+package bridzelabz.SnakeLadderProblem;
+
+import java.util.Random;
+
+public class SnakeLadder {
+	public static void main(String[] args) {
+	System.out.println("Start at 0");
+	//Random rm=new Random();
+	//int die=rm.nextInt(6)+1;
+	//System.out.println("The value on die is:" +die);
+	
+	}
+}
